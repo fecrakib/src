8153f688ac63a61139a598ce5800ce42fab1b886
fecrakib <rakibislam132575@gmail.com>
2024-05-15 16:56:53 +0600

    params

--- steam.js
+++ steam.js
@@ -1,11 +1,36 @@
 
 const express  = require('express');
+const handle  = require('./handle');
 const app  = express();
 const port  = 3000;
-app.get('/',(req,res)=>{
+
+app.param('id',(req,res,next,id)=>{
+    const user  = {
+        userId:id,
+        name:"Bangladesh"
+    };
+    req.userDetails=user;
+    next();
+})
+
+app.get('/user/:id',(req,res)=>{
+    console.log(req.userDetails)
     res.send("hello world");
 })
+
+app.use(express.raw())
+
+const admin  = express();
+admin.get('/dashboard',(req,res)=>{
+    res.send("welcome to admin dashboard")
+})
  
+admin.use(express.raw())
+app.use('/admin',admin)
+
+app.locals.title="my app is"
+app.post('/',handle);
 app.listen(port,()=>{
+    console.log(app.locals.title)
     console.log("sever is running");
 })
